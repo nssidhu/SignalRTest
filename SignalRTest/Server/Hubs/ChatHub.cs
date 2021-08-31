9c415da526eb01b5be9eb4609e4048896268094d
Naunihal Singh Sidhu <nssidhu@outlook.com>
2021-08-31 18:06:20 -0500

    Working

---
+++ SignalRTest/Server/Hubs/ChatHub.cs
@@ -0,0 +1,29 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.SignalR;
+
+namespace SignalRTest.Server.Hubs
+{
+    //[Authorize]
+    public class ChatHub : Hub
+    {
+        public async Task SendMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+
+        public override async Task OnConnectedAsync()
+        {
+            var name = Context.User.Identity.Name;
+            Clients.All.SendAsync("ReceiveMessage", "system", $"{Context.ConnectionId} joined the conversation");
+            await base.OnConnectedAsync();
+        }
+
+        public override async Task OnDisconnectedAsync(Exception exception)
+        {
+            var name = Context.User.Identity.Name;
+            Clients.All.SendAsync("ReceiveMessage", "system", $"{Context.ConnectionId} left the conversation");
+            await base.OnDisconnectedAsync(exception);
+        }
+    }
+}
--- SignalRTest/Server/Program.cs
+++ SignalRTest/Server/Program.cs
@@ -1,10 +1,26 @@
 ﻿using Microsoft.AspNetCore.ResponseCompression;
-using SignalRTest.Server.Hub;
+using SignalRTest.Server.Hubs;
+using Microsoft.AspNetCore.SignalR;
+using Microsoft.AspNetCore.Http.Connections;
 
 var builder = WebApplication.CreateBuilder(args);
 
+
+//https://docs.microsoft.com/en-us/aspnet/core/signalr/authn-and-authz?view=aspnetcore-5.0#bearer-token-authentication
+
 // Add services to the container.
-builder.Services.AddSignalR();
+//builder.Services.AddSignalR();
+builder.Services.AddSignalR(hubOptions =>
+{
+    //https://docs.microsoft.com/en-us/aspnet/signalr/overview/guide-to-the-api/handling-connection-lifetime-events
+    hubOptions.EnableDetailedErrors = true;
+    //hubOptions.KeepAliveInterval = TimeSpan.FromMinutes(1);
+});
+//builder.Services.AddSignalR(srConfig => srConfig.EnableDetailedErrors = true)
+//        .AddAzureSignalR(azureConfig =>
+//        {
+//            azureConfig.ConnectionString = "your connection string";
+//        });
 builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages();
 builder.Services.AddResponseCompression(opts =>
@@ -37,12 +53,31 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+//var webSocketOptions = new Microsoft.AspNetCore.Builder.WebSocketOptions()
+//{
+//    KeepAliveInterval = TimeSpan.FromSeconds(120),
+//};
+//webSocketOptions.AllowedOrigins.Add("https://client.com");
+//webSocketOptions.AllowedOrigins.Add("https://www.client.com");
+
+//app.UseWebSockets(webSocketOptions);
 
 app.UseEndpoints(endpoints =>
 {
     endpoints.MapRazorPages();
     endpoints.MapControllers();
-    endpoints.MapHub<ChatHub>("/chathub");
+    //endpoints.MapHub<ChatHub>("/chathub");
+    endpoints.MapHub<ChatHub>("/chathub", options =>
+    {
+        //https://docs.microsoft.com/en-us/aspnet/core/signalr/configuration?view=aspnetcore-2.1&tabs=dotnet
+        //options.Transports = TransportType.All;
+        options.Transports = HttpTransportType.WebSockets | HttpTransportType.LongPolling;
+        options.TransportMaxBufferSize = 256000;
+        options.ApplicationMaxBufferSize = 256000;
+       
+        //options.WebSockets.CloseTimeout = TimeSpan.FromSeconds(10);
+        //options.LongPolling.PollTimeout = TimeSpan.FromSeconds(10);
+    });
     endpoints.MapFallbackToFile("index.html");
 });
 
