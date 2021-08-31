0bb454bfb5c34ac525af802350e4a0368d81f479
Naunihal Singh Sidhu <nssidhu@outlook.com>
2021-08-30 22:41:46 -0500

    Add project files.

---
+++ SignalRTest/Server/Hub/ChatHub.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace SignalRTest.Server.Hub;
+public class ChatHub : Microsoft.AspNetCore.SignalR.Hub
+{
+    public async Task SendMessage(string user, string message)
+    {
+        await Clients.All.SendAsync("ReceiveMessage", user, message);
+    }
+}
---
+++ SignalRTest/Server/Program.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.ResponseCompression;
+using SignalRTest.Server.Hub;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddSignalR();
+builder.Services.AddControllersWithViews();
+builder.Services.AddRazorPages();
+builder.Services.AddResponseCompression(opts =>
+{
+    opts.MimeTypes = ResponseCompressionDefaults.MimeTypes.Concat(
+        new[] { "application/octet-stream" });
+});
+
+var app = builder.Build();
+
+app.UseResponseCompression();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+    app.UseWebAssemblyDebugging();
+}
+else
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseBlazorFrameworkFiles();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapRazorPages();
+    endpoints.MapControllers();
+    endpoints.MapHub<ChatHub>("/chathub");
+    endpoints.MapFallbackToFile("index.html");
+});
+
+//app.MapRazorPages();
+//app.MapControllers();
+//app.MapFallbackToFile("index.html");
+
+app.Run();
