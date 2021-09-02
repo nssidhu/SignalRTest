9ad766f95f2dd4f88378afd68add69eb0ead5547
Naunihal Singh Sidhu <nssidhu@outlook.com>
2021-09-01 19:10:19 -0500

    Save

--- SignalRTest/Server/Hubs/ChatHub.cs
+++ SignalRTest/Server/Hubs/ChatHub.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading.Tasks;
+﻿using System.Security.Claims;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.SignalR;
 
@@ -16,6 +17,10 @@ namespace SignalRTest.Server.Hubs
         {
             var name = Context.User.Identity.Name;
             Clients.All.SendAsync("ReceiveMessage", "system", $"{Context.ConnectionId} joined the conversation");
+
+            //string name = Context.User.Identity.Name;
+            //Groups.Add(Context.ConnectionId, name);
+
             await base.OnConnectedAsync();
         }
 
@@ -24,6 +29,31 @@ namespace SignalRTest.Server.Hubs
             var name = Context.User.Identity.Name;
             Clients.All.SendAsync("ReceiveMessage", "system", $"{Context.ConnectionId} left the conversation");
             await base.OnDisconnectedAsync(exception);
+        }
+
+        //https://docs.microsoft.com/en-us/aspnet/core/signalr/groups?view=aspnetcore-5.0
+        public async Task AddToGroup(string groupName)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, groupName);
+
+            await Clients.Group(groupName).SendAsync("Send", $"{Context.ConnectionId} has joined the group {groupName}.");
+        }
+
+        public async Task RemoveFromGroup(string groupName)
+        {
+            await Groups.RemoveFromGroupAsync(Context.ConnectionId, groupName);
+
+            await Clients.Group(groupName).SendAsync("Send", $"{Context.ConnectionId} has left the group {groupName}.");
+        }
+    }
+
+
+    //https://docs.microsoft.com/en-us/aspnet/core/signalr/authn-and-authz?view=aspnetcore-5.0#use-claims-to-customize-identity-handling
+    public class EmailBasedUserIdProvider : IUserIdProvider
+    {
+        public virtual string GetUserId(HubConnectionContext connection)
+        {
+            return connection.User?.FindFirst(ClaimTypes.Email)?.Value;
         }
     }
 }
--- SignalRTest/Server/Program.cs
+++ SignalRTest/Server/Program.cs
@@ -2,11 +2,62 @@
 using SignalRTest.Server.Hubs;
 using Microsoft.AspNetCore.SignalR;
 using Microsoft.AspNetCore.Http.Connections;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 
 var builder = WebApplication.CreateBuilder(args);
 
 
 //https://docs.microsoft.com/en-us/aspnet/core/signalr/authn-and-authz?view=aspnetcore-5.0#bearer-token-authentication
+
+
+builder.Services.AddSingleton<IUserIdProvider, EmailBasedUserIdProvider>();
+
+//https://docs.microsoft.com/en-us/aspnet/core/signalr/authn-and-authz?view=aspnetcore-5.0
+//builder.Services.AddAuthentication(options =>
+//{
+//    // Identity made Cookie authentication the default.
+//    // However, we want JWT Bearer Auth to be the default.
+//    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+//    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+//})
+//        .AddJwtBearer(options =>
+//        {
+//            // Configure the Authority to the expected value for your authentication provider
+//            // This ensures the token is appropriately validated
+//            // options.Authority = /* TODO: Insert Authority URL here */;
+
+//            // We have to hook the OnMessageReceived event in order to
+//            // allow the JWT authentication handler to read the access
+//            // token from the query string when a WebSocket or 
+//            // Server-Sent Events request comes in.
+
+//            // Sending the access token in the query string is required due to
+//            // a limitation in Browser APIs. We restrict it to only calls to the
+//            // SignalR hub in this code.
+//            // See https://docs.microsoft.com/aspnet/core/signalr/security#access-token-logging
+//            // for more information about security considerations when using
+//            // the query string to transmit the access token.
+//            options.Events = new JwtBearerEvents
+//            {
+//                OnMessageReceived = context =>
+//                {
+//                    var accessToken = context.Request.Query["access_token"];
+
+//                    // If the request is for our hub...
+//                    var path = context.HttpContext.Request.Path;
+//                    if (!string.IsNullOrEmpty(accessToken) &&
+//                        (path.StartsWithSegments("/hubs/chat")))
+//                    {
+//                        // Read the token out of the query string
+//                        context.Token = accessToken;
+//                    }
+//                    return Task.CompletedTask;
+//                }
+//            };
+//        });
+
+
+
 
 // Add services to the container.
 //builder.Services.AddSignalR();
@@ -53,6 +104,7 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+//https://docs.microsoft.com/en-us/aspnet/core/fundamentals/websockets?view=aspnetcore-5.0#websocket-origin-restriction
 //var webSocketOptions = new Microsoft.AspNetCore.Builder.WebSocketOptions()
 //{
 //    KeepAliveInterval = TimeSpan.FromSeconds(120),
